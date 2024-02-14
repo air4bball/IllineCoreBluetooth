108086b9c2f27841c4eef6ee3d55f82b870fe23e
srikarp <air4bball@gmail.com>
2024-02-14 00:25:30 -0600

    renamed from CoreBluetoothTest to IllineCoreBluetooth

--- IllineCoreBluetoothUITests/IllineCoreBluetoothUITests.swift
+++ IllineCoreBluetoothUITests/IllineCoreBluetoothUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class CoreBluetoothTestUITests: XCTestCase {
+final class IllineCoreBluetoothUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- IllineCoreBluetoothUITests/IllineCoreBluetoothUITestsLaunchTests.swift
+++ IllineCoreBluetoothUITests/IllineCoreBluetoothUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class CoreBluetoothTestUITestsLaunchTests: XCTestCase {
+final class IllineCoreBluetoothUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
